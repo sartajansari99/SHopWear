7dec0065f6715c1d2bd2b5ea49738a3f3742aa2a
sartajansari99 <132349164+sartajansari99@users.noreply.github.com>
2024-02-26 11:35:38 +0530

    updated

--- backends/index.js
+++ backends/index.js
@@ -1,4 +1,4 @@
-const port = require('https://shopwear.onrender.com/');
+const port = 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -43,7 +43,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/${req.file.filename}`,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
   });
 });
 
